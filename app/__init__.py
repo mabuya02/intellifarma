cafe272250b85fe77f48161f15a3729ebca35a95
mabuya02 <mainamanasseh02@gmail.com>
2023-12-02 00:47:58 +0300

    minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
-from flask import Flask, app
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask , app
+from flask_mail import Mail
 from config import Config
 from flask_restful import Api
-from flask_mail import Mail
-
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 api = Api()
